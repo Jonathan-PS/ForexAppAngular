3e106e94aa6944986c7ccd579b301d981319cb85
JonathanPS <jonathanpstang@gmail.com>
2019-03-28 10:41:49 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Exchange } from "./exchange";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ForexAppAngular';
+export class AppComponent implements OnInit {
+  title = "ForexAppAngular";
+  results: Exchange[];
+  interval: number;
+  timer: number;
+  progress: number;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.refreshData();
+    this.progress = 0;
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(() => {
+      this.refreshData();
+      this.progress = 0;
+    }, 10000);
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+    this.timer = setInterval(() => {
+      this.progress += 5;
+    }, 500);
+  }
+
+  refreshData(): void {
+    this.http
+      .get<Exchange[]>(
+        "https://forex.1forge.com/1.0.3/quotes?pairs=EURNOK,GBPNOK,USDNOK,CADNOK,CHFNOK,AUDNOK,NZDNOK,ZARNOK,NOKZAR&api_key=9bW5urPmwxXJnFB2hxzyfmi7JX9kuIUN"
+      )
+      .subscribe(
+        data => {
+          this.results = data;
+        },
+        err => {
+          console.log("Error fetching data.");
+        }
+      );
+  }
 }
